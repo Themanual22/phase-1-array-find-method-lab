86e22e3792aad98a8c4e4de69414b63e38b3dad9
Themanual22 <djamosry22@gmail.com>
2022-02-03 11:01:29 -0500

    Completed assignment1

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
-// code your solution here
-superbowlWin = (record) => {
-  const result = record.find( record => record.result === "W" );
-  return !!result ? result.year : undefined;
+// // code your solution here
+// superbowlWin = (record) => {
+//   const result = record.find( record => record.result === "W" );
+//   return !!result ? result.year : undefined;
+// }
+const record = [
+  { year: "2015", result: "W"},
+  { year:"2014", result: "N/A"},
+  { year:"2013", result: "L"},
+]
+
+function superbowlWin(array) {
+  let helpMe = array.find( element => element.result === "W")
+  if (helpMe === undefined) {
+    return undefined
+  } else {
+    return helpMe.year
+  }
 }
